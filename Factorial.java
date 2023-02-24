408f78901b1ab8655d66f39283b6683473c00b28
Yashashree  Bedmutha <yashashree.bedmutha@gmail.com>
2023-02-24 21:56:09 +0530

    Input Validation Included

--- Factorial.java
+++ Factorial.java
@@ -1,17 +1,43 @@
 import java.util.Scanner;
 
-public class Factorial {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter a positive integer:");
-
-    int number = sc.nextInt();
-    long factorial = 1;
-
-    for (int i = 1; i <= number; i++) {
-      factorial = factorial * i;
+public class A2 
+{
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n;
+            while (true) {
+                try {
+                    System.out.print("Enter a non-negative integer to calculate its factorial: ");
+                    n = scanner.nextInt();
+                    if (n < 0) 
+                    {
+                        throw new IllegalArgumentException("Error: the number must be non-negative");
+                    }
+                    break;
+                } 
+                catch (IllegalArgumentException ex) 
+                {
+                    System.out.println(ex.getMessage());
+                } 
+                catch (Exception ex) 
+                {
+                    System.out.println("Error: invalid input, please enter a non-negative integer");
+                    scanner.nextLine(); // clear the input buffer
+                }
+            }
+            long factorial = calculateFactorial(n);
+            System.out.printf("%d! = %d", n, factorial);
+        }
     }
 
-    System.out.println("Factorial of " + number + " is: " + factorial);
-  }
+    private static long calculateFactorial(int n) 
+    {
+        long factorial = 1;
+        for (int i = 2; i <= n; i++) 
+        {
+            factorial *= i;
+        }
+        return factorial;
+    }
 }
+
